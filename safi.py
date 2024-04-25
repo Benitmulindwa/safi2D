84cc3c768d025ae17fbbb1782a295f7e34ccacf4
Benit Mulindwa <110304380+Benitmulindwa@users.noreply.github.com>
2024-04-25 22:57:53 +0200

    Update safi.py

--- safi.py
+++ safi.py
@@ -1,20 +1,20 @@
 # Fill the pixel buffer with colors
 pixels = [[(0, 0, 0) for _ in range(400)] for _ in range(400)]
 
-# Fill the pixel buffer with a red background
+# Fill the pixel buffer with a while background
 for i in range(400):
     for j in range(400):
         pixels[i][j] = (255, 255, 255)
 
 
-def draw_rectangle(x1, y1, x2, y2):
+def draw_rectangle(x1:int, y1:int, x2:int, y2:int, color:tuple):
     for i in range(max(0, y1 - 1), min(len(pixels), y2 + 1)):
         for j in range(max(0, x1 - 1), min(len(pixels[0]), x2 + 1)):
-            pixels[i][j] = (255, 0, 0)
+            pixels[i][j] = color
 
 
 # Save the pixel buffer to a PPM file
-draw_rectangle(10, 100, 300, 300)
+draw_rectangle(10, 100, 300, 300, color=(255, 0, 0))
 with open("output.ppm", "w") as f:
     f.write("P3\n")
     f.write(f"{400} {400}\n")
