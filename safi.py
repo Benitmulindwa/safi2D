bbbf47ef31feef19487edcfeaf5c588bcb33488b
Benit Mulindwa <110304380+Benitmulindwa@users.noreply.github.com>
2024-04-25 19:38:45 +0200

    Update safi.py

--- safi.py
+++ safi.py
@@ -7,14 +7,14 @@ for i in range(400):
         pixels[i][j] = (255, 255, 255)
 
 
-def draw_rectangle():
-    for i in range(100, 300):
-        for j in range(100, 300):
+def draw_rectangle(x1, y1, x2, y2):
+    for i in range(max(0, y1 - 1), min(len(pixels), y2 + 1)):
+        for j in range(max(0, x1 - 1), min(len(pixels[0]), x2 + 1)):
             pixels[i][j] = (255, 0, 0)
 
 
 # Save the pixel buffer to a PPM file
-draw_rectangle()
+draw_line(0, 0, 100, 100)
 with open("output.ppm", "w") as f:
     f.write("P3\n")
     f.write(f"{400} {400}\n")
