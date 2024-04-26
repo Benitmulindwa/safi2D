896e4c1fcf03ef8b13cc557ee83b90284ff8e802
Benitmulindwa <mulindwabenit@gmail.com>
2024-04-26 02:23:34 +0200

    create examples folder

---
+++ examples/rect.py
@@ -0,0 +1,14 @@
+from safi import Canvas
+
+rows = 10
+cols = 10
+img = Canvas("dame.ppm", 400, 400, bgcolor=(255, 0, 255))
+# img.draw_rectangle(100, 100, 300, 300, color=(0, 0, 0))
+for y in range(0, rows):
+    for x in range(0, cols):
+        if (x + y) % 2 == 0:
+            color = (255, 0, 0)
+        else:
+            color = (0, 255, 0)
+        img.draw_rectangle(x * (400 // rows), y * (400 // rows), 400, 400, color=color)
+img.save()
